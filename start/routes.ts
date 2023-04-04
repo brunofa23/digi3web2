93feb2a060e54e8745d72a882f6f255247679c9e
Bruno Favato <brunofa23@gmail.com>
2023-04-04 15:05:11 -0300

    cors maxage: null - v36

--- start/routes.ts
+++ start/routes.ts
@@ -7,7 +7,7 @@ const logtail = new Logtail("2QyWC3ehQAWeC6343xpMSjTQ");
 Route.group(() => {
 
   Route.get('/', async () => {
-    return { hello: 'Hello World v35' }
+    return { hello: 'Hello World v36' }
   })
 
   //BOOKS
