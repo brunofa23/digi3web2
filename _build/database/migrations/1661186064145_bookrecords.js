2222cf8837067e12d6df5e0c8710b223b8218fa3
Bruno Favato <brunofa23@gmail.com>
2023-05-08 12:57:50 +0000

    chore(release): 1.1.74

--- _build/database/migrations/1661186064145_bookrecords.js
+++ _build/database/migrations/1661186064145_bookrecords.js
@@ -11,6 +11,7 @@ class default_1 extends Schema_1.default {
     }
     async up() {
         this.schema.createTable(this.tableName, (table) => {
+            table.primary(['id', 'typebooks_id', 'books_id', 'companies_id']);
             table.increments('id');
             table.integer('typebooks_id').notNullable().unsigned().references('typebooks.id');
             table.integer('books_id').notNullable().unsigned().references('typebooks.books_id').onDelete('CASCADE');
