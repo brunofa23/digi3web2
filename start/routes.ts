9c104a4289ee4d20cd372003e62077e29104978e
Bruno Favato <brunofa23@gmail.com>
2023-10-23 11:20:33 -0300

    V-80

--- start/routes.ts
+++ start/routes.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', async () => {
-    return { hello: 'Sistema de Gerenciamento de Imagens - v79' }
+    return { hello: 'Sistema de Gerenciamento de Imagens - v80' }
   })//.middleware('level_permission:1')
 
   //BOOK
