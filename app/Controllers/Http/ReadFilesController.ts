28a333b19d375b7cb9e33ace63bf4fb43f3b2b3a
Bruno Favato <brunofa23@gmail.com>
2024-07-03 17:52:07 -0300

    documentos

--- app/Controllers/Http/ReadFilesController.ts
+++ app/Controllers/Http/ReadFilesController.ts
@@ -9,7 +9,10 @@ export default class ReadFilesController {
 
   public async readFile({ auth, request, params, response }: HttpContextContract) {
     const authenticate = await auth.use('api').authenticate()
-    //console.log("readfile....", authenticate)
+    const {typebooks_id, books_id, companies_id}=request.only(['typebooks_id', 'books_id', 'companies_id'])
+
+    console.log("readfile....", typebooks_id, books_id, companies_id)
+
     const file = request.file('file', {
       size: '100mb',
       extnames: ['xls', 'xlsx', 'csv']
@@ -23,14 +26,12 @@ export default class ReadFilesController {
     const filePath = Application.tmpPath(`/uploads/Client_${authenticate.companies_id}/${file.clientName}`)
     const bookrecords = await readFile(filePath)
 
-    //console.log('FILE>>', bookrecords)
-
     for (const bookrecord of bookrecords) {
-      const searchPayload = { id: bookrecord.id, typebooks_id:53, books_id:13, companies_id:10 }
+      const searchPayload = { id: bookrecord.id, typebooks_id:typebooks_id, books_id:books_id, companies_id:companies_id }
       const persistanceBookrecord = { id: bookrecord.id, typebooks_id:53, books_id:13, companies_id:10 }
       const retorno =  await Bookrecord.updateOrCreate(searchPayload, persistanceBookrecord)
-      console.log("retorno", retorno.id)
-      
+      //console.log("retorno", retorno.id)
+
 
     }
 
