d5560326504e689f01d0cb239b4839ce9473441d
Bruno Favato <brunofa23@gmail.com>
2024-07-19 17:09:39 +0000

    chore(release): 1.1.199

--- _build/app/Services/fileRename/fileRename.js
+++ _build/app/Services/fileRename/fileRename.js
@@ -235,7 +235,7 @@ async function fileRename(originalFileName, typebooks_id, companies_id, dataImag
             .whereRaw(query).first();
         if (bookRecord === null)
             return;
-        const seq = bookRecord?.indeximage.length + 1;
+        const seq = bookRecord.indeximage[bookRecord.indeximage.length - 1].seq + 1;
         let fileRename;
         try {
             fileRename = {
