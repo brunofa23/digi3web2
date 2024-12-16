02992092edf7cea7dce0eaee4a338e6ace4aee68
Bruno Favato <brunofa23@gmail.com>
2024-12-16 12:49:37 -0300

    ajustes site

---
+++ app/Controllers/Http/MailmanangersController.ts
@@ -0,0 +1,38 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Mail from '@ioc:Adonis/Addons/Mail'
+
+export default class MailmanangersController {
+
+  public async sendMailContactWebsite({ request,response }: HttpContextContract) {
+    const {name, email, phone, body} = request.only(['name', 'email', 'phone', 'body'])
+    try {
+            //Enviar por email
+            const sendmail = await Mail.send((message) => {
+                message.from('no-reply@mgcartorios.com.br')
+                    .to("brunofa23@gmail.com")
+                    .cc("favatoamaral@gmail.com")
+                    .cc("sistemasdigi3@gmail.com")
+                    .priority("high")
+                    .subject('Formulário de Contato - Digi3')
+                    .htmlView('emails/contacts', {
+                        name: name,
+                        email: email,
+                        phone: phone,
+                        body: body
+                    })
+            })
+
+            return response.status(201).send(sendmail)
+
+        } catch (error) {
+            return error
+        }
+
+
+}
+
+
+
+
+
+}
