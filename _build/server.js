85fe3fa29c609ae3569c63f53a60974bead8e39c
Bruno Favato <brunofa23@gmail.com>
2025-10-23 22:37:21 +0000

    chore(build): update build [skip ci]

--- _build/server.js
+++ _build/server.js
@@ -6,18 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const source_map_support_1 = __importDefault(require("source-map-support"));
 const standalone_1 = require("@adonisjs/core/build/standalone");
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
-const tmpPath = path_1.default.join(__dirname, 'tmp', 'uploads');
-try {
-    if (!fs_1.default.existsSync(tmpPath)) {
-        fs_1.default.mkdirSync(tmpPath, { recursive: true });
-        console.log('📁 Pasta tmp/uploads criada:', tmpPath);
-    }
-}
-catch (err) {
-    console.error('❌ Erro ao criar pasta tmp/uploads:', err);
-}
 source_map_support_1.default.install({ handleUncaughtExceptions: false });
-new standalone_1.Ignitor(__dirname).httpServer().start();
+new standalone_1.Ignitor(__dirname)
+    .httpServer()
+    .start();
 //# sourceMappingURL=server.js.map
