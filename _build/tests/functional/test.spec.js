a864d200c164969ebd3fb902ddcb9dec4957d8b2
Bruno Favato <brunofa23@gmail.com>
2025-10-24 17:22:56 +0000

    chore(build): update build [skip ci]

--- _build/tests/functional/test.spec.js
+++ _build/tests/functional/test.spec.js
@@ -8,8 +8,7 @@ const Application_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core
 const processImage_1 = global[Symbol.for('ioc.use')]("App/Services/imageProcessing/processImage");
 (0, runner_1.test)('test', async ({ client }) => {
     const inputImage = Application_1.default.tmpPath('/test2/ImagemLivro.jpg');
-    const outputImage = Application_1.default.tmpPath('/test2/ImagemLivroAlterada.jpg');
-    const result = await (0, processImage_1.processImage)(inputImage, outputImage);
+    const result = await (0, processImage_1.processImage)(inputImage);
     console.log('✅ OK ->', result);
 });
 //# sourceMappingURL=test.spec.js.map
